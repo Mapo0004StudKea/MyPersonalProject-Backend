b5f12a25b6732371d672a3e42f09ff16db6b27c9
Martin Poulsen <mapo0004@stud.kea.dk>
2024-12-13 10:42:44 +0100

    no changes

---
+++ src/main/java/stud/kea/dk/mypersonalprojectbackend/MyPersonalProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package stud.kea.dk.mypersonalprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPersonalProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPersonalProjectBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/stud/kea/dk/mypersonalprojectbackend/MyPersonalProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package stud.kea.dk.mypersonalprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPersonalProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
